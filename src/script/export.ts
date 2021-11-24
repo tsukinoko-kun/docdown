675fb46ea60e2a45021f30cec6b089218b7fa223
Frank Mayer <frank.mayer1907@protonmail.com>
2021-11-24 23:04:11 +0100

    pdf export

---
+++ src/script/export.ts
@@ -0,0 +1,22 @@
+import { context } from "./context";
+import html2pdf from "html2pdf.js";
+
+const pdf = html2pdf().set({
+  margin: 1,
+  jsPDF: { unit: "cm", format: "A4", orientation: "portrait" },
+});
+
+const displayEl = document.getElementById("display") as HTMLDivElement;
+
+displayEl.addEventListener("contextmenu", (ev) => {
+  ev.preventDefault();
+
+  context(ev, [
+    {
+      display: "Export PDF",
+      action: () => {
+        pdf.from(displayEl.innerHTML).save();
+      },
+    },
+  ]);
+});
