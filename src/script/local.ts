fef5bc7445cfb7df0b352e397be13dcbdd9fe45f
Frank Mayer <frank.mayer1907@protonmail.com>
2021-11-27 01:50:17 +0100

    added localisation strings

--- src/script/local.ts
+++ src/script/local.ts
@@ -13,7 +13,12 @@ interface ILanguageMap {
   stop_session: string;
   add_source: string;
   delete_source: string;
+  use_source: string;
   last_accessed_at: string;
+  find: string;
+  replace: string;
+  replace_all: string;
+  not_found: string;
 }
 
 const localStringMap: { [key in language]: ILanguageMap } = {
@@ -26,11 +31,16 @@ const localStringMap: { [key in language]: ILanguageMap } = {
     last_access: "Letzter Zugriff",
     link: "Link",
     export_pdf: "PDF exportieren",
-    start_new_session: "Neue Sitzung starten",
+    start_new_session: "Sitzung starten",
     stop_session: "Sitzung beenden",
     add_source: "Quelle hinzufügen",
     delete_source: "Quelle löschen",
+    use_source: "Quelle verlinken",
     last_accessed_at: "Zuletzt zugegriffen am",
+    find: "Suchen",
+    replace: "Ersetzen",
+    replace_all: "Alle ersetzen",
+    not_found: "Nicht gefunden",
   },
   en: {
     sources: "Sources",
@@ -41,15 +51,22 @@ const localStringMap: { [key in language]: ILanguageMap } = {
     last_access: "Last access",
     link: "Link",
     export_pdf: "Export to PDF",
-    start_new_session: "Start new session",
+    start_new_session: "Start a session",
     stop_session: "Stop session",
     add_source: "Add source",
     delete_source: "Delete source",
+    use_source: "Use source",
     last_accessed_at: "Last accessed at",
+    find: "Find",
+    replace: "Replace",
+    replace_all: "Replace all",
+    not_found: "Not found",
   },
 };
 
-const currentLanguage: language = "de";
+const currentLanguage: language = navigator.language.includes("de")
+  ? "de"
+  : "en";
 
 export const getLocale = (): language => currentLanguage;
 
