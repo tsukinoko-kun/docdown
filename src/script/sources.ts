b76158aefd18c1e13789708d49e85308e154c0b2
Frank Mayer <frank.mayer1907@protonmail.com>
2021-11-29 21:20:41 +0100

    switched xxhash implementation

--- src/script/sources.ts
+++ src/script/sources.ts
@@ -4,7 +4,7 @@ import {
   addDisposableEventListener,
   disposeNode,
 } from "@frank-mayer/magic/bin";
-import xxhash from "xxhash-wasm";
+import { h64 } from "xxhashjs";
 import { userAlert, userForm } from "./alert";
 import { getLocale, getLocalizedString } from "./local";
 
@@ -12,6 +12,8 @@ import type { ContextOption } from "./context";
 
 const sourcesEl = document.getElementById("sources") as HTMLUListElement;
 const codeEl = document.getElementById("code") as HTMLTextAreaElement;
+
+const hashSeed = 0x12345678;
 
 interface ISourceData {
   id: string;
@@ -152,132 +154,130 @@ export const importSourcesJSON = (sources: IterableIterator<ISourceData>) => {
 
 export const sourceTag = /(?<=<src>)[0-9a-z]+(?=<\/src>)/g;
 
-xxhash().then((xxh) => {
-  class SourceData implements ISourceData {
-    id: string;
-    author: string;
-    title: string;
-    creationDate: number | null;
-    lastAccessed: number;
-    link: string;
+class SourceData implements ISourceData {
+  id: string;
+  author: string;
+  title: string;
+  creationDate: number | null;
+  lastAccessed: number;
+  link: string;
 
-    constructor(
-      author: string,
-      title: string,
-      creationDate: number | null,
-      lastAccessed: number,
-      link: string
-    ) {
-      this.author = author;
-      this.title = title;
-      this.creationDate = creationDate;
-      this.lastAccessed = lastAccessed;
-      this.link = link;
+  constructor(
+    author: string,
+    title: string,
+    creationDate: number | null,
+    lastAccessed: number,
+    link: string
+  ) {
+    this.author = author;
+    this.title = title;
+    this.creationDate = creationDate;
+    this.lastAccessed = lastAccessed;
+    this.link = link;
 
-      this.id = Number.parseInt(
-        "0x" +
-          xxh.h64(`${author}-${title}-${creationDate}-${lastAccessed}-${link}`)
-      ).toString(36);
-    }
-
-    static from(data: {
-      author: string;
-      title: string;
-      creationDate: string | null;
-      lastAccessed: string;
-      link: string;
-    }) {
-      return new SourceData(
-        data.author,
-        data.title,
-        data.creationDate ? new Date(data.creationDate).getTime() : null,
-        new Date(data.lastAccessed).getTime(),
-        data.link
-      );
-    }
+    this.id = h64(
+      `${author}-${title}-${creationDate}-${lastAccessed}-${link}`,
+      hashSeed
+    ).toString(36);
   }
 
-  const contextOptionAdd: ContextOption = {
-    label: getLocalizedString("add_source"),
-    action: () => {
-      userForm([
-        {
-          name: "author",
-          label: getLocalizedString("author"),
-          required: true,
-          type: "text",
-        },
-        {
-          name: "title",
-          label: getLocalizedString("title"),
-          required: true,
-          type: "text",
-        },
-        {
-          name: "creationDate",
-          label: getLocalizedString("creation_date"),
-          required: false,
-          type: "date",
-        },
-        {
-          name: "lastAccessed",
-          label: getLocalizedString("last_access"),
-          required: true,
-          type: "date",
-        },
-        {
-          name: "link",
-          label: getLocalizedString("link"),
-          required: true,
-          type: "string",
-        },
-      ])
-        .then((data) => {
-          const sourceData = SourceData.from(data);
-          sourcesRegister.set(sourceData.id, sourceData);
-          sourcesEl.appendChild(createLiFromSource(sourceData));
-        })
-        .catch((err) => {
-          if (err) {
-            console.error(err);
-          }
-        });
-    },
-  };
+  static from(data: {
+    author: string;
+    title: string;
+    creationDate: string | null;
+    lastAccessed: string;
+    link: string;
+  }) {
+    return new SourceData(
+      data.author,
+      data.title,
+      data.creationDate ? new Date(data.creationDate).getTime() : null,
+      new Date(data.lastAccessed).getTime(),
+      data.link
+    );
+  }
+}
 
-  sourcesEl.addEventListener(
-    "contextmenu",
-    (ev) => {
-      ev.preventDefault();
-      ev.stopPropagation();
-
-      let tEl = ev.target as HTMLElement | null;
-      while (tEl) {
-        if (tEl.tagName === "LI") {
-          context(ev, [
-            contextOptionAdd,
-            {
-              label: getLocalizedString("delete_source"),
-              action: () => {
-                sourcesRegister.delete(tEl!.id);
-                deleteAllSubstringsInText(codeEl, `<src>${tEl!.id}</src>`);
-                disposeNode(tEl!);
-                triggerRender();
-              },
-            },
-          ]);
-
-          return;
+const contextOptionAdd: ContextOption = {
+  label: getLocalizedString("add_source"),
+  action: () => {
+    userForm([
+      {
+        name: "author",
+        label: getLocalizedString("author"),
+        required: true,
+        type: "text",
+      },
+      {
+        name: "title",
+        label: getLocalizedString("title"),
+        required: true,
+        type: "text",
+      },
+      {
+        name: "creationDate",
+        label: getLocalizedString("creation_date"),
+        required: false,
+        type: "date",
+      },
+      {
+        name: "lastAccessed",
+        label: getLocalizedString("last_access"),
+        required: true,
+        type: "date",
+      },
+      {
+        name: "link",
+        label: getLocalizedString("link"),
+        required: true,
+        type: "string",
+      },
+    ])
+      .then((data) => {
+        const sourceData = SourceData.from(data);
+        sourcesRegister.set(sourceData.id, sourceData);
+        sourcesEl.appendChild(createLiFromSource(sourceData));
+      })
+      .catch((err) => {
+        if (err) {
+          console.error(err);
         }
+      });
+  },
+};
 
-        tEl = tEl.parentElement;
+sourcesEl.addEventListener(
+  "contextmenu",
+  (ev) => {
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    let tEl = ev.target as HTMLElement | null;
+    while (tEl) {
+      if (tEl.tagName === "LI") {
+        context(ev, [
+          contextOptionAdd,
+          {
+            label: getLocalizedString("delete_source"),
+            action: () => {
+              sourcesRegister.delete(tEl!.id);
+              deleteAllSubstringsInText(codeEl, `<src>${tEl!.id}</src>`);
+              disposeNode(tEl!);
+              triggerRender();
+            },
+          },
+        ]);
+
+        return;
       }
 
-      context(ev, [contextOptionAdd]);
-    },
-    {
-      capture: true,
-      passive: false,
+      tEl = tEl.parentElement;
     }
-  );
-});
+
+    context(ev, [contextOptionAdd]);
+  },
+  {
+    capture: true,
+    passive: false,
+  }
+);
