3a33197990b99736b4736a6b44f9451d0b0e16ce
Frank Mayer <frank.mayer1907@protonmail.com>
2021-11-30 17:44:21 +0100

    fixed some cors and priority issues

--- src/sw.js
+++ src/sw.js
@@ -1,30 +1,69 @@
 const CACHE_NAME = "sw-cache-v1";
 
+const clearCache = (async () => {
+  const cache = await caches.open(CACHE_NAME);
+  const storedRequests = await cache.keys();
+  for (const req of storedRequests) {
+    console.debug("delete from cache", req);
+  }
+})();
+
+/** @type {RequestInit} */
+const CORS = {
+  mode: "no-cors",
+  cache: "default",
+  method: "GET",
+};
+
+/** @type {CacheQueryOptions}  */
+const cacheQueryOptions = {
+  ignoreMethod: true,
+  ignoreSearch: true,
+};
+
+/**
+ * Try get from server, if fail, try get from cache
+ * @param {RequestInfo} url
+ * @returns {Promise<Response>}
+ */
+const findSource = async (url) => {
+  await clearCache;
+
+  const urlStr = url.toString();
+  const cache = await caches.open(CACHE_NAME);
+
+  console.debug("findSource", url);
+
+  if (urlStr.startsWith(location.origin)) {
+    if (navigator.onLine) {
+      if (!urlStr.endsWith(".html") && !urlStr.endsWith("/")) {
+        console.debug("try cache", url);
+        try {
+          return await cache.match(url, cacheQueryOptions);
+        } catch (e) {
+          console.debug("cache error", e);
+          console.debug("try fetch", url);
+          const data = await fetch(url, CORS);
+          await cache.put(url, data.clone());
+          return data;
+        }
+      }
+
+      console.debug("try fetch", url);
+      const data = await fetch(url, CORS);
+      await cache.put(url, data.clone());
+      return data;
+    } else {
+      console.debug("offline, try cache", url);
+      return await cache.match(url, cacheQueryOptions);
+    }
+  } else {
+    console.debug("cross origin, try fetch", url);
+    return await fetch(url, CORS);
+  }
+};
+
 self.addEventListener("fetch", (event) => {
   // @ts-ignore
-  event.respondWith(
-    new Promise((resolve, reject) => {
-      // @ts-ignore
-      const url = event.request.url;
-
-      fetch(url)
-        .then((response) => {
-          if (response.status >= 200 && response.status < 300) {
-            if (url.beginsWith(self.location.origin)) {
-              caches.open(CACHE_NAME).then((cache) => {
-                cache.put(url, response.clone());
-              });
-            }
-            resolve(response);
-          } else {
-            reject(response);
-          }
-        })
-        .catch(() => {
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.match(url).then(resolve).catch(reject);
-          });
-        });
-    })
-  );
+  event.waitUntil(findSource(event.request.url));
 });
