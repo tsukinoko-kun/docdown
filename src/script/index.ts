e39495cf3fd036abc7391078f25f76c140bcf11c
Frank Mayer <frank.mayer1907@protonmail.com>
2021-12-02 17:08:20 +0100

    use one single index module

---
+++ src/script/index.ts
@@ -0,0 +1,21 @@
+/// <reference path="global.d.ts" />
+
+export * as alert from "./alert";
+export * as database from "./database";
+export * as dataHelper from "./dataHelper";
+export * as dracula from "./dracula";
+export * as editor from "./editor";
+export * as escape from "./escape";
+export * as export from "./export";
+export * as local from "./local";
+export * as passive from "./passive";
+export * as pdfStylesheet from "./pdfStylesheet";
+export * as render from "./render";
+export * as session from "./session";
+export * as shortcuts from "./shortcuts";
+export * as sources from "./sources";
+export * as statistics from "./statistics";
+export * as syncScroll from "./syncScroll";
+export * as theme from "./theme";
+export * as tutorial from "./tutorial";
+export * as upload from "./upload";
