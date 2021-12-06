12a772f90ce5fb8178f631e98334dd8668f2407a
Frank Mayer <frank.mayer1907@protonmail.com>
2021-12-06 11:32:04 +0100

    added notify functionality after message

--- src/script/router.ts
+++ src/script/router.ts
@@ -9,6 +9,7 @@ import type { IContextData } from "./ui/alert";
 import type { ISessionData } from "./logic/session";
 import type { themeId, ITheme } from "./logic/theme";
 import type { pdfOutput } from "./logic/export";
+import type { language } from "./data/local";
 
 /**
  * module service
@@ -26,6 +27,8 @@ export enum service {
   logout,
   context,
   createPdf,
+  setLocale,
+  serHeaderText,
 }
 
 enum paramResult {
@@ -53,9 +56,12 @@ interface IServiceMap {
   [service.logout]: ParamResult<undefined, void>;
   [service.context]: ParamResult<IContextData, void>;
   [service.createPdf]: ParamResult<pdfOutput, void>;
+  [service.setLocale]: ParamResult<language, void>;
+  [service.serHeaderText]: ParamResult<string, void>;
 }
 
 const messageReciever = new Map<service, Array<Function>>();
+const messageNotifyReciever = new Map<service, Array<Function>>();
 
 export const listenForMessage = <S extends keyof IServiceMap>(
   service: S,
@@ -71,6 +77,18 @@ export const listenForMessage = <S extends keyof IServiceMap>(
   }
 };
 
+export const notifyOnMessage = <S extends keyof IServiceMap>(
+  service: S,
+  callback: (messageResult: IServiceMap[S][paramResult.result]) => void
+) => {
+  const s = messageNotifyReciever.get(service);
+  if (s) {
+    s.push(callback);
+  } else {
+    messageNotifyReciever.set(service, [callback]);
+  }
+};
+
 export const sendMessage = <
   S extends keyof IServiceMap,
   R extends IServiceMap[S][paramResult.result]
@@ -80,7 +98,12 @@ export const sendMessage = <
 ) => {
   const s = messageReciever.get(service);
   if (s && s.length > 0) {
-    return Some<R>(s.map((cb) => cb(message))[0]);
+    const value = s.map((cb) => cb(message))[0];
+    const s2 = messageNotifyReciever.get(service);
+    if (s2 && s2.length > 0) {
+      s2.forEach((cb) => cb(value));
+    }
+    return Some<R>(value);
   } else {
     return None<R>();
   }
