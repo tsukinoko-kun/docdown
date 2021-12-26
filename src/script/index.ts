0e0053ca656ffff8b5207a8f6acf8588f81a85b5
Frank Mayer <frank.mayer1907@protonmail.com>
2021-12-26 19:54:38 +0100

    added splash screen

--- src/script/index.ts
+++ src/script/index.ts
@@ -2,3 +2,19 @@ export * as router from "./router";
 export * as data from "./data";
 export * as ui from "./ui";
 export * as logic from "./logic";
+
+const removeSplash = () => {
+  for (const el of Array.from(document.getElementsByClassName("splash"))) {
+    el.remove();
+  }
+};
+
+const windowLoaded = (window as any).load as Promise<void> | undefined;
+
+if (windowLoaded) {
+  windowLoaded.then(() => {
+    removeSplash();
+  });
+} else {
+  removeSplash();
+}
