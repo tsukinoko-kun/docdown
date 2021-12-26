4dae928eb612baeb3bad8e571a515cbb32f34a3f
Frank Mayer <frank.mayer1907@protonmail.com>
2021-12-26 15:52:55 +0100

    tooltip bugfix position & added shortcut to tooltip

--- src/script/ui/tooltip.ts
+++ src/script/ui/tooltip.ts
@@ -1,36 +1,73 @@
-import { html, addDisposableEventListener } from "@frank-mayer/magic";
+import { addDisposableEventListener } from "@frank-mayer/magic";
 
-export const tooltip = (el: Element, text: string) => {
+const platform: string =
+  navigator.platform || (navigator as any).userAgentData.platform;
+
+const CMD = (text: string) => {
+  return text.replace(
+    /CMD/gi,
+    platform.match(/(Mac|iPhone|iPod|iPad)/i) ? "⌘" : "Ctrl"
+  );
+};
+
+export const tooltip = (
+  el: Element,
+  text: string,
+  fixed: boolean = false,
+  shortcut?: string
+) => {
   const rect = el.getBoundingClientRect();
 
   const x = rect.left + rect.width / 2;
   const y = rect.top + rect.height + 14;
 
-  const tt = html`<div
-    class="ct ct--bottom ct--shown"
-    style="left: ${x}px; top: ${y}px; transform: translateX(-50%)"
-  >
-    <div class="ct__content">
-      <div>
-        ${text}
-        <!-- <div class="ce-toolbar__plus-shortcut">⇥ Tab</div> -->
-      </div>
-    </div>
-  </div>`[0]!;
+  const ttEl = document.createElement("div");
+  ttEl.classList.add("ct");
+  ttEl.classList.add("ct--bottom");
+  ttEl.style.left = `${x}px`;
+  ttEl.style.top = `${y}px`;
+  ttEl.style.transform = "translateX(-50%)";
+  if (fixed) {
+    ttEl.style.position = "fixed";
+  }
+
+  const contentEl = document.createElement("div");
+  contentEl.classList.add("ct__content");
+  ttEl.appendChild(contentEl);
+
+  const contentInnerEl = document.createElement("div");
+  contentInnerEl.innerText = text;
+  contentEl.appendChild(contentInnerEl);
+
+  if (shortcut) {
+    const shortcutEl = document.createElement("div");
+    shortcutEl.classList.add("ce-toolbar__plus-shortcut");
+    shortcutEl.innerText = CMD(shortcut);
+    contentInnerEl.appendChild(shortcutEl);
+  }
+
+  document.body.appendChild(ttEl);
 
   addDisposableEventListener(el, "mousemove", () => {
-    document.body.appendChild(tt);
+    ttEl.classList.add("ct--shown");
   });
 
   addDisposableEventListener(el, "mouseout", () => {
-    if (tt.parentElement === document.body) {
-      document.body.removeChild(tt);
-    }
+    ttEl.classList.remove("ct--shown");
   });
 };
 
 // add custom tooltip to all elements
 document.body.querySelectorAll("[title]").forEach((el) => {
-  tooltip(el, (el as HTMLElement).title);
+  const title = el.getAttribute("title");
+  if (!title) {
+    return;
+  }
+
+  const shortcut = el.getAttribute("shortcut");
+
+  tooltip(el, title, true, shortcut as string);
+
   el.removeAttribute("title");
+  el.removeAttribute("shortcut");
 });
